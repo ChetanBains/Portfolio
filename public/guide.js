7a90408c1e75c3de869ae6047ce96ab3e7f2a8d2
Chetanjeev Singh Bains <>
2023-07-23 22:24:16 +0530

    Code Updated

--- public/guide.js
+++ public/guide.js
@@ -1,12 +1,7 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const close = document.querySelector('.header .nav-bar .nav-list ul');
-
-// close.addEventListener('click', () => {
-//     mobile_menu.classList.collapse('hide');
-// });
-
-
+// Use descriptive variable names
+const hamburger = document.querySelector('.header .nav-bar .hamburger .bar');
+const mobile_menu = document.querySelector('.header .nav-list ul');
+const header = document.querySelector('#header');
 
 hamburger.addEventListener('click', () => {
     mobile_menu.classList.toggle('active');
